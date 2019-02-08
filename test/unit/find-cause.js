a57dfee1fe857809963de6e32fc9c7f3a5e58107
Steve Ripberger <sripberger@batterii.com>
2019-02-08 15:19:51 -0500

    Support error classes as predicates in findCause

--- test/unit/find-cause.js
+++ test/unit/find-cause.js
@@ -2,16 +2,20 @@ import * as internal from '../../lib/internal';
 import { findCause } from '../../lib/find-cause';
 
 describe('findCause', function() {
-	it('finds cause in error by predicate', function() {
+	it('finds cause in error by normalized predicate', function() {
 		const err = new Error('err');
 		const cause = new Error('err cause');
 		const predicate = () => {};
+		const normalized = () => {};
+		sinon.stub(internal, 'normalizePredicate').returns(normalized);
 		sinon.stub(internal, 'findCauseByPredicate').returns(cause);
 
 		const result = findCause(err, predicate);
 
+		expect(internal.normalizePredicate).to.be.calledOnce;
+		expect(internal.normalizePredicate).to.be.calledWith(predicate);
 		expect(internal.findCauseByPredicate).to.be.calledOnce;
-		expect(internal.findCauseByPredicate).to.be.calledWith(err, predicate);
+		expect(internal.findCauseByPredicate).to.be.calledWith(err, normalized);
 		expect(result).to.equal(cause);
 	});
 });
--- test/unit/internal.js
+++ test/unit/internal.js
@@ -1,5 +1,6 @@
+import * as isModule from '../../lib/is';
 import * as iterateCausesModule from '../../lib/iterate-causes';
-import { findCauseByPredicate } from '../../lib/internal';
+import { findCauseByPredicate, normalizePredicate } from '../../lib/internal';
 
 describe('internal functions', function() {
 	describe('findCauseByPredicate', function() {
@@ -52,6 +53,53 @@ describe('internal functions', function() {
 			const result = findCauseByPredicate(err, predicate);
 
 			expect(result).to.be.null;
+		});
+	});
+
+	describe('normalizePredicate', function() {
+		const predicate = () => {};
+
+		beforeEach(function() {
+			sinon.stub(isModule, 'is').returns(false);
+		});
+
+		it('checks if the predicate is an Error class', function() {
+			normalizePredicate(predicate);
+
+			expect(isModule.is).to.be.calledOnce;
+			expect(isModule.is).to.be.calledWith(Error, predicate);
+		});
+
+		it('returns predicate, if it is not an Error class', function() {
+			const result = normalizePredicate(predicate);
+
+			expect(result).to.equal(predicate);
+		});
+
+		it('returns a different function, if predicate is an Error class', function() {
+			isModule.is.returns(true);
+
+			const result = normalizePredicate(predicate);
+
+			expect(result).to.be.a('function');
+			expect(result).to.not.equal(predicate);
+		});
+
+		describe('function returned when predicate is an Error class', function() {
+			it('returns result of is with predicate and provided error', function() {
+				const err = new Error('Omg bad error!');
+				const isResult = 'result of is function';
+				isModule.is.returns(true);
+				const fn = normalizePredicate(predicate);
+				isModule.is.reset();
+				isModule.is.returns(isResult);
+
+				const result = fn(err);
+
+				expect(isModule.is).to.be.calledOnce;
+				expect(isModule.is).to.be.calledWith(predicate, err);
+				expect(result).to.equal(isResult);
+			});
 		});
 	});
 });
