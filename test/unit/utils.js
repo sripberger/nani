50e526149a074325b810df825425c299ed0b7aba
Steve Ripberger <sripberger@batterii.com>
2019-02-08 11:08:18 -0500

    Move iterate util into its own module

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -52,38 +52,4 @@ describe('utils', function() {
 			expect(result).to.deep.equal({});
 		});
 	});
-
-	describe('::iterate', function() {
-		it('iterates through all causes of an error', function() {
-			const fooErr = new Error('foo');
-			const barErr = fooErr.cause = new Error('bar');
-			const bazErr = barErr.cause = new Error('baz');
-
-			const result = Array.from(utils.iterate(fooErr));
-
-			expect(result).to.deep.equal([ fooErr, barErr, bazErr ]);
-		});
-
-		it('iterates through errors property instead of cause, if any', function() {
-			const fooErr = new Error('foo');
-			const barErr = fooErr.cause = new Error('bar');
-			const bazErr = new Error('baz');
-			fooErr.errors = [ barErr, bazErr ];
-
-			const result = Array.from(utils.iterate(fooErr));
-
-			expect(result).to.deep.equal([ fooErr, barErr, bazErr ]);
-		});
-
-		it('iterates causes of errors in errors property', function() {
-			const fooErr = new Error('foo');
-			const barErr = fooErr.cause = new Error('bar');
-			const bazErr = barErr.cause = Error('baz');
-			fooErr.errors = [ barErr ];
-
-			const result = Array.from(utils.iterate(fooErr));
-
-			expect(result).to.deep.equal([ fooErr, barErr, bazErr ]);
-		});
-	});
 });
