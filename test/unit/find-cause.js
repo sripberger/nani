d69dad0212356306ff3f0e5985f4e9a2060eab4a
Steve Ripberger <sripberger@batterii.com>
2019-02-08 14:23:48 -0500

    Refactor main findCause logic into internal module

--- test/unit/find-cause.js
+++ test/unit/find-cause.js
@@ -1,55 +1,17 @@
-import * as iterateCausesModule from '../../lib/iterate-causes';
+import * as internal from '../../lib/internal';
 import { findCause } from '../../lib/find-cause';
 
 describe('findCause', function() {
-	let err, fooErr, barErr, bazErr, predicate;
-
-	beforeEach(function() {
-		err = new Error('Original error');
-		fooErr = new Error('foo');
-		barErr = new Error('bar');
-		bazErr = new Error('baz');
-		predicate = sinon.stub().named('predicate').returns(false);
-
-		sinon.stub(iterateCausesModule, 'iterateCauses').returns({
-			*[Symbol.iterator]() {
-				yield fooErr;
-				yield barErr;
-				yield bazErr;
-			},
-		});
-	});
-
-	it('iterates over causes of err', function() {
-		findCause(err, predicate);
-
-		expect(iterateCausesModule.iterateCauses).to.be.calledOnce;
-		expect(iterateCausesModule.iterateCauses).to.be.calledWith(err);
-	});
-
-	it('invokes predicate with each cause', function() {
-		findCause(err, predicate);
-
-		expect(predicate).to.be.calledThrice;
-		expect(predicate).to.be.calledWith(fooErr);
-		expect(predicate).to.be.calledWith(barErr);
-		expect(predicate).to.be.calledWith(bazErr);
-	});
-
-	it('returns first cause where predicate returns true', function() {
-		predicate.withArgs(barErr).returns(true);
+	it('finds cause in error by predicate', function() {
+		const err = new Error('err');
+		const cause = new Error('err cause');
+		const predicate = () => {};
+		sinon.stub(internal, 'findCauseByPredicate').returns(cause);
 
 		const result = findCause(err, predicate);
 
-		expect(predicate).to.be.calledTwice;
-		expect(predicate).to.be.calledWith(fooErr);
-		expect(predicate).to.be.calledWith(barErr);
-		expect(result).to.equal(barErr);
-	});
-
-	it('returns null if predicate never returns true', function() {
-		const result = findCause(err, predicate);
-
-		expect(result).to.be.null;
+		expect(internal.findCauseByPredicate).to.be.calledOnce;
+		expect(internal.findCauseByPredicate).to.be.calledWith(err, predicate);
+		expect(result).to.equal(cause);
 	});
 });
