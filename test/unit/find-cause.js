1e3f2764be83008dd2a190fdfe543418dd3603b3
Steve Ripberger <sripberger@batterii.com>
2019-02-11 15:19:14 -0500

    Move filterCauses into module with findCause

--- test/unit/find-cause.js
+++ test/unit/find-cause.js
@@ -1,28 +1,53 @@
 import * as findCauseInternalModule from '../../lib/find-cause-internal';
 import * as normalizePredicateModule from '../../lib/normalize-predicate';
-import { findCause } from '../../lib/find-cause';
+import { filterCauses, findCause } from '../../lib/find-cause';
 
-describe('findCause', function() {
-	it('finds cause in error by normalized predicate', function() {
-		const err = new Error('err');
-		const cause = new Error('err cause');
-		const predicate = () => {};
-		const normalized = () => {};
-		const normalizePredicate = sinon.stub(
+describe('External cause-finding functions', function() {
+	const err = new Error('err');
+	const predicate = () => {};
+	const normalized = () => {};
+	let normalizePredicate;
+
+	beforeEach(function() {
+		normalizePredicate = sinon.stub(
 			normalizePredicateModule,
 			'normalizePredicate'
 		).returns(normalized);
-		const findCauseByPredicate = sinon.stub(
-			findCauseInternalModule,
-			'findCauseByPredicate'
-		).returns(cause);
+	});
 
-		const result = findCause(err, predicate);
+	describe('findCause', function() {
+		it('finds cause in error by normalized predicate', function() {
+			const cause = new Error('err cause');
+			const findCauseByPredicate = sinon.stub(
+				findCauseInternalModule,
+				'findCauseByPredicate'
+			).returns(cause);
 
-		expect(normalizePredicate).to.be.calledOnce;
-		expect(normalizePredicate).to.be.calledWith(predicate);
-		expect(findCauseByPredicate).to.be.calledOnce;
-		expect(findCauseByPredicate).to.be.calledWith(err, normalized);
-		expect(result).to.equal(cause);
+			const result = findCause(err, predicate);
+
+			expect(normalizePredicate).to.be.calledOnce;
+			expect(normalizePredicate).to.be.calledWith(predicate);
+			expect(findCauseByPredicate).to.be.calledOnce;
+			expect(findCauseByPredicate).to.be.calledWith(err, normalized);
+			expect(result).to.equal(cause);
+		});
+	});
+
+	describe('filterCauses', function() {
+		it('filters causes of error by normalized predicate', function() {
+			const causes = [ new Error('foo'), new Error('bar') ];
+			const filterCausesByPredicate = sinon.stub(
+				findCauseInternalModule,
+				'filterCausesByPredicate'
+			).returns(causes);
+
+			const result = filterCauses(err, predicate);
+
+			expect(normalizePredicate).to.be.calledOnce;
+			expect(normalizePredicate).to.be.calledWith(predicate);
+			expect(filterCausesByPredicate).to.be.calledOnce;
+			expect(filterCausesByPredicate).to.be.calledWith(err, normalized);
+			expect(result).to.equal(causes);
+		});
 	});
 });
