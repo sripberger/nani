512f311636056cfcbb0860c6b81344fc8da06e15
Steve Ripberger <sripberger@batterii.com>
2019-02-23 15:10:22 -0500

    Add iterateFull external function

--- test/unit/iterate-external.js
+++ test/unit/iterate-external.js
@@ -1,30 +1,52 @@
+import * as iterateInternalModule from '../../lib/iterate-internal';
 import * as iteratorModule from '../../lib/iterator';
-import { iterate } from '../../lib/iterate-external';
+import { iterate, iterateFull } from '../../lib/iterate-external';
 
 describe('External iterate utils', function() {
 	describe('iterate', function() {
 		it('iterates error using a new Iterator', function() {
-			const err = new Error('Original error');
 			const fooErr = new Error('foo');
 			const barErr = new Error('bar');
 			const bazErr = new Error('baz');
 			const iterator = sinon.createStubInstance(iteratorModule.Iterator);
 			iterator.iterate.returns({
 				*[Symbol.iterator]() {
-					yield fooErr;
 					yield barErr;
 					yield bazErr;
 				},
 			});
 			sinon.stub(iteratorModule, 'Iterator').returns(iterator);
 
-			const result = [ ...iterate(err) ];
+			const results = [ ...iterate(fooErr) ];
 
 			expect(iteratorModule.Iterator).to.be.calledOnce;
 			expect(iteratorModule.Iterator).to.be.calledWithNew;
 			expect(iterator.iterate).to.be.calledOnce;
-			expect(iterator.iterate).to.be.calledWith(err);
-			expect(result).to.deep.equal([ fooErr, barErr, bazErr ]);
+			expect(iterator.iterate).to.be.calledWith(fooErr);
+			expect(results).to.deep.equal([ barErr, bazErr ]);
+		});
+	});
+
+	describe('iterateFull', function() {
+		it('iterates error using iterateInternal directly', function() {
+			const err = new Error('Omg bad error!');
+			const result1 = { foo: 'bar' };
+			const result2 = { ba: 'qux' };
+			const iterateInternal = sinon.stub(
+				iterateInternalModule,
+				'iterateInternal'
+			).returns({
+				*[Symbol.iterator]() {
+					yield result1;
+					yield result2;
+				},
+			});
+
+			const results = [ ...iterateFull(err) ];
+
+			expect(iterateInternal).to.be.calledOnce;
+			expect(iterateInternal).to.be.calledWithExactly(err);
+			expect(results).to.deep.equal([ result1, result2 ]);
 		});
 	});
 });
