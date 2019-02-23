5975aaebd6c5e9c925f100cf15b1cd0c70877331
Steve Ripberger <sripberger@batterii.com>
2019-02-23 14:44:01 -0500

    Refactor Iterator#iterate to use iterateInternal

--- test/unit/iterator.js
+++ test/unit/iterator.js
@@ -1,3 +1,4 @@
+import * as iterateInternalModule from '../../lib/iterate-internal';
 import { Iterator } from '../../lib/iterator';
 
 describe('Iterator', function() {
@@ -7,53 +8,44 @@ describe('Iterator', function() {
 		iterator = new Iterator();
 	});
 
-	it('creates an empty set for storing iterated errors', function() {
-		expect(iterator.errors).to.deep.equal(new Set());
+	it('creates an empty set for storing seen errors', function() {
+		expect(iterator.seenErrors).to.deep.equal(new Set());
 	});
 
 	describe('#iterate', function() {
-		it('iterates through all causes of an error, storing each in errors set', function() {
-			const fooErr = new Error('foo');
-			const barErr = fooErr.cause = new Error('bar');
-			const bazErr = barErr.cause = new Error('baz');
+		let fooErr, barErr, bazErr, iterateInternal;
 
-			const result = [ ...iterator.iterate(fooErr) ];
+		beforeEach(function() {
+			fooErr = new Error('foo');
+			barErr = new Error('bar');
+			bazErr = new Error('baz');
 
-			expect(result).to.deep.equal([ fooErr, barErr, bazErr ]);
-			expect(iterator.errors).to.have.keys(fooErr, barErr, bazErr);
+			iterateInternal = sinon.stub(
+				iterateInternalModule,
+				'iterateInternal'
+			).returns({
+				*[Symbol.iterator]() {
+					yield { err: barErr };
+					yield { err: bazErr };
+				},
+			});
 		});
 
-		it('iterates through errors property instead of cause, if any', function() {
-			const fooErr = new Error('foo');
-			const barErr = fooErr.cause = new Error('bar');
-			const bazErr = new Error('baz');
-			fooErr.errors = [ barErr, bazErr ];
+		it('yields errors from iterateInternal, storing each in seenErrors', function() {
+			const results = [ ...iterator.iterate(fooErr) ];
 
-			const result = [ ...iterator.iterate(fooErr) ];
-
-			expect(result).to.deep.equal([ fooErr, barErr, bazErr ]);
-			expect(iterator.errors).to.have.keys(fooErr, barErr, bazErr);
+			expect(iterateInternal).to.be.calledOnce;
+			expect(iterateInternal).to.be.calledWithExactly(fooErr);
+			expect(results).to.deep.equal([ barErr, bazErr ]);
+			expect(iterator.seenErrors).to.have.keys(barErr, bazErr);
 		});
 
-		it('iterates causes of errors in errors property', function() {
-			const fooErr = new Error('foo');
-			const barErr = fooErr.cause = new Error('bar');
-			const bazErr = barErr.cause = Error('baz');
-			fooErr.errors = [ barErr ];
+		it('skips errors that are already in seenErrors', function() {
+			iterator.seenErrors.add(barErr);
 
-			const result = [ ...iterator.iterate(fooErr) ];
+			const results = [ ...iterator.iterate(fooErr) ];
 
-			expect(result).to.deep.equal([ fooErr, barErr, bazErr ]);
-			expect(iterator.errors).to.have.keys(fooErr, barErr, bazErr);
-		});
-
-		it('yields nothing if error is in errors set', function() {
-			const err = new Error('whatver');
-			iterator.errors.add(err);
-
-			const result = [ ...iterator.iterate(err) ];
-
-			expect(result).to.be.empty;
+			expect(results).to.deep.equal([ bazErr ]);
 		});
 	});
 });
